b13ee92a932a1b6b85e9aff0278829743fb2a9d9
钟佳林 <zhongjialin@zhongjialindeMacBook-2.local>
2016-01-16 19:43:20 +0800

    2016/1/16

---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+s = sys.path
+for x in s:
+	print(x)
---
+++ untitled.py
@@ -0,0 +1,6 @@
+journey = 10
+mile = journey / 1.61
+time = (43 * 60 +30)/ (60*60)
+average = mile / time
+print("average is", average)
+      
